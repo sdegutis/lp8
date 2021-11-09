237a4afb52b4cbfc4eeb1902fea19dca97355481
Steven Degutis <sbdegutis@gmail.com>
2021-11-09 15:26:12 -0600

    Scaling pico8 images

--- main.lua
+++ main.lua
@@ -1,15 +1,20 @@
 local lib = require("lib")
+local sprs
 
-local sprs = lib.parseFile("test1.p8")
+function love.load()
+  love.graphics.setDefaultFilter("nearest", "nearest")
+  love.window.setMode(800, 600)
+  sprs = lib.parseFile("test1.p8")
+end
 
 function love.draw()
   love.graphics.clear()
-  love.graphics.draw(sprs[0],  10, 10)
-  love.graphics.draw(sprs[1],  20, 10)
-  love.graphics.draw(sprs[2],  30, 10)
-  love.graphics.draw(sprs[3],  40, 10)
-  love.graphics.draw(sprs[17], 50, 10)
-  love.graphics.draw(sprs[18], 60, 10)
-  love.graphics.draw(sprs[19], 70, 10)
-  love.graphics.draw(sprs[37], 80, 10)
+  love.graphics.draw(sprs[0],  10*3, 10, 0, 2, 2)
+  love.graphics.draw(sprs[1],  20*3, 10, 0, 2, 2)
+  love.graphics.draw(sprs[2],  30*3, 10, 0, 2, 2)
+  love.graphics.draw(sprs[3],  40*3, 10, 0, 2, 2)
+  love.graphics.draw(sprs[17], 50*3, 10, 0, 2, 2)
+  love.graphics.draw(sprs[18], 60*3, 10, 0, 2, 2)
+  love.graphics.draw(sprs[19], 70*3, 10, 0, 2, 2)
+  love.graphics.draw(sprs[37], 80*3, 10, 0, 2, 2)
 end
